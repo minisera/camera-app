8a49b4331c8e7a36e1e876e0f956a9ee9b52abce
minisera <koki.ohtaka0821@gmail.com>
2022-04-30 17:06:35 +0900

    Initial Commit

---
+++ camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  camera
+//
+//  Created by 光気 on 2022/04/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
